9f8e555f370c2dabd0d9be5124a91fd2281b7796
Ryan Stillings <rystills@gmail.com>
2017-09-03 05:19:16 -0400

    fix bugs in player movement
    subtract gridX/Y to get the x/y position relative to the current grid
    space, rather than using modular division, as the latter causes issues
    when x/yprev still fall under the previous grid square. Update grid
    values again before checking if we can accomodate our want direction
    after moving.

--- Player.js
+++ Player.js
@@ -32,28 +32,31 @@ Player.prototype.update = function() {
 		this.y += Math.sign(this.direction - 2) * deltaTime * this.speed;
 	}
 	
-	//update the grid value again now that we've moved
-	gridVal = grid[this.gridY][this.gridX];
 	//check if we can resolve a discrepancy between the desired direction and the actual direction
 	if (this.wantDirection != this.direction) {
+		//update the grid value and grid x and y immediately for use in calculations
+		this.gridX = Math.floor(this.x / gridWidth);
+		this.gridY = Math.floor(this.y / gridHeight);
+		gridVal = grid[this.gridY][this.gridX];
+		
 		//before setting the direction, check if our new grid space still allows that movement
-		if (gridVal == 3 || gridVal == (i % 2 ? 2 : 1)) {
+		if (gridVal == 3 || gridVal == (this.wantDirection % 2 ? 2 : 1)) {
 			//if we crossed over the center of a gridspace, we can initiate a direction change
 			if (!(this.direction%2)) {
 				//want to go from horizontal to vertical
-				if (((this.x % gridWidth >= gridWidth/2) && (this.xPrev % gridWidth <= gridWidth/2)) ||
-				((this.x % gridWidth <= gridWidth/2) && (this.xPrev % gridWidth >= gridWidth/2))) {
+				if (((this.x - this.gridX*gridWidth >= gridWidth/2) && (this.xPrev - this.gridX*gridWidth <= gridWidth/2)) ||
+				((this.x - this.gridX*gridWidth <= gridWidth/2) && (this.xPrev - this.gridX*gridWidth >= gridWidth/2))) {
 					//move to the center of the gridSpace, and change direction
-					this.x -= (this.x % gridWidth - gridWidth/2);
+					this.x -= (this.x - this.gridX*gridWidth - gridWidth/2);
 					this.direction = this.wantDirection;
 				}
 			}
 			else {
 				//want to go from vertical to horizontal
-				if (((this.y % gridHeight >= gridHeight/2) && (this.yPrev % gridHeight <= gridHeight/2)) ||
-				((this.y % gridHeight <= gridHeight/2) && (this.yPrev % gridHeight >= gridHeight/2))) {
+				if (((this.y - this.gridY*gridHeight >= gridHeight/2) && (this.yPrev - this.gridY*gridHeight <= gridHeight/2)) ||
+				((this.y - this.gridY*gridHeight <= gridHeight/2) && (this.yPrev - this.gridY*gridHeight >= gridHeight/2))) {
 					//move to the center of the gridSpace, and change direction
-					this.y -= (this.y % gridHeight - gridHeight/2);
+					this.y -= (this.y - this.gridY*gridHeight - gridHeight/2);
 					this.direction = this.wantDirection;
 				}
 			}
